992c599c6d426fec1c29050d5f4a0ca92e9e9963
Yasmine Hartung <y.hartung@gmail.com>
2019-03-19 15:18:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,11 @@
 def my_select(collection)
  i = 0
-
+ j = 0
+ true_collection
  while i < collection.length do
-
+   if yield collection [i]
+     true_collection[j] = collection [i]
+     j = j + 1
    i = i + 1
  end
 end
